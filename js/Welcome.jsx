e2c3d845e9b32581b25a94940c963b074d0cfa78
Richard Leek <richard@richardleek.com>
2021-01-10 17:35:58 -0500

    add links for shop, item, and master files

--- js/Welcome.jsx
+++ js/Welcome.jsx
@@ -3,25 +3,28 @@ import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import FileUpload from './FileUpload';
 
-const pubTypesNeedingEif = ['drop'];
+const pubTypesNeedingEcf = ['item'];
+const pubTypesNeedingEif = ['drop', 'shop'];
 const pubTypesNeedingEnf = ['drop', 'talk'];
+const pubTypesNeedingEsf = ['master'];
 
 function Welcome(props) {
   const [valid, setValid] = useState(false);
   const [file, setFile] = useState(undefined);
   const [file2, setFile2] = useState(undefined);
   const [file3, setFile3] = useState(undefined);
+  const [needEcf, setNeedEcf] = useState(false);
   const [needEif, setNeedEif] = useState(false);
   const [needEnf, setNeedEnf] = useState(false);
+  const [needEsf, setNeedEsf] = useState(false);
 
   useEffect(() => {
     if (file) {
       let pubType = utils.getPubType(file.name);
-      let needEif = pubTypesNeedingEif.includes(pubType);
-      let needEnf = pubTypesNeedingEnf.includes(pubType);
-      setNeedEif(needEif);
-      setNeedEnf(needEnf);
-
+      setNeedEcf(pubTypesNeedingEcf.includes(pubType));
+      setNeedEif(pubTypesNeedingEif.includes(pubType));
+      setNeedEnf(pubTypesNeedingEnf.includes(pubType));
+      setNeedEsf(pubTypesNeedingEsf.includes(pubType));
       setValid(true);
     } else {
       setValid(false);
@@ -37,7 +40,7 @@ function Welcome(props) {
   const onSecondarySelected = file => {
     if (!file2) {
       setFile2(file);
-    } else {
+    } else if (!file3) {
       setFile3(file);
     }
   };
@@ -55,6 +58,13 @@ function Welcome(props) {
         message="To get things started please select the pub file you would like to modify"
         accept=".ecf, .eif, .eid, .esf, .emf, .edf, .enf, .etf"
         onFileSelected={onMainSelected} />
+      {needEcf &&
+        <FileUpload
+          message="It looks like the file you're trying to edit contains references
+        to a class (ecf) file. If you want the class names to show up please
+        upload this file below."
+          accept=".ecf"
+          onFileSelected={onSecondarySelected} />}
       {needEif &&
         <FileUpload
           message="It looks like the file you're trying to edit contains references
@@ -68,6 +78,13 @@ function Welcome(props) {
           to an npc (enf) file. If you want the npc names to show up please
         upload this file below."
           accept=".enf"
+          onFileSelected={onSecondarySelected} />}
+      {needEsf &&
+        <FileUpload
+          message="It looks like the file you're trying to edit contains references
+          to a spell (esf) file. If you want the spell names to show up please
+        upload this file below."
+          accept=".esf"
           onFileSelected={onSecondarySelected} />}
 
       <Button variant="primary" disabled={!valid} onClick={onSubmit}>Submit</Button>
